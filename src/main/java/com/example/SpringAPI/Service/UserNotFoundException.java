6803b1c9aa11ba5e70a3ce5423e0e2cf811e133d
emiku06 <148997595+emiku06@users.noreply.github.com>
2024-06-21 14:29:48 +0300

    Update UserNotFoundException.java
    If no users say so

--- src/main/java/com/example/SpringAPI/Service/UserNotFoundException.java
+++ src/main/java/com/example/SpringAPI/Service/UserNotFoundException.java
@@ -4,4 +4,7 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(int id) {
         super("Could not find user " + id);
     }
+    public UserNotFoundException() {
+        super("No users");
+    }
 }
